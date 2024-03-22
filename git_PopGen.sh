6e75bd575702c999fbe7eb8ef1f450100258fab5
HooiMin <tangrahn@gmail.com>
2024-03-22 15:01:16 +0100

    update

--- git_PopGen.sh
+++ git_PopGen.sh
@@ -1,6 +1,6 @@
 # set up a new repository from remote to local
 git clone git@github.com:hooimin7/BINP29_ANCIENTLINK.git
-cd BINP29_Malaria/
+cd BINP29_ANCIENTLINK
 git add README.md 
 git branch
 git commit -m "first commit"
@@ -9,10 +9,8 @@ git branch
 # git remote add origin git@github.com:hooimin7/BINP29_Malaria.git # add the remote repository
 
 git push -u origin main
-cd BINP29_Malaria/
-cp ../.gitignore .
+cd BINP29_ANCIENTLINK
 ls -a
-git add .gitignore 
 git commit -m "files"
 git branch
 git push origin main
